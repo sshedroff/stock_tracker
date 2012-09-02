390b445c3f22c52550fc0576e6d92323b0e79e7d
Brandon Rice <brice84@gmail.com>
2012-09-02 14:59:53 -0400

    Fix formatting.

--- src/net/neverone/stocktracker/Tracker.java
+++ src/net/neverone/stocktracker/Tracker.java
@@ -17,71 +17,71 @@ import java.io.*;
 import java.util.*;
 
 public class Tracker {
-	
+
 	private URL url;
 	private ArrayList<Stock> stocks;
-	
+
 	public static void main(String[] args) {
 		Tracker tracker = new Tracker();
-		
-		for (Stock stock: tracker.stocks) {
+
+		for (Stock stock : tracker.stocks) {
 			System.out.println(stock);
 		}
 	}
-	
+
 	public Tracker() {
 		String urlString = "http://download.finance.yahoo.com/d/quotes.csv?s=";
 		File file = new File("stocks.txt");
 		Scanner fileReader;
 		stocks = new ArrayList<Stock>();
-		
+
 		try {
 			fileReader = new Scanner(file);
 			String newSymbol;
-			
+
 			while (fileReader.hasNextLine()) {
 				stocks.add(new Stock(newSymbol = fileReader.nextLine()));
 				urlString += newSymbol + ",";
 			}
-			
+
 			stocks.trimToSize();
 		} catch (FileNotFoundException e) {
 			System.err.println("File not found.");
 			System.exit(1);
 		}
-		
+
 		urlString += "&f=nl1&e=.csv"; // needed to finish off the URL.
-		
+
 		try {
 			url = new URL(urlString);
 		} catch (MalformedURLException e) {
 			System.err.println("Bad URL.");
 			System.exit(1);
 		}
-		
+
 		update();
 	} // end Tracker constructor
-	
+
 	public void update() {
 		BufferedReader in;
 		String inputLine;
 		int counter = 0;
-		
+
 		try {
 			in = new BufferedReader(new InputStreamReader(url.openStream()));
-			
+
 			while ((inputLine = in.readLine()) != null) {
 				String[] tokenString = inputLine.split(",");
 				stocks.get(counter).name = tokenString[0];
 				stocks.get(counter).value = Double.parseDouble(tokenString[1]);
 				counter++;
 			}
-			
+
 			in.close();
 		} catch (IOException e) {
 			System.err.println("I/O Exception while reading.");
 			System.exit(1);
-		} 
+		}
 	} // end update
-	
+
 } // end Tracker
